31212b997e94aa46e0de1019957e807e3c03a766
Snigdha Sur <Snigdha@Snigdhas-MacBook-Air-2.local>
2017-07-17 10:19:42 -0400

    Done.

---
+++ app/application.rb
@@ -0,0 +1,30 @@
+class Application 
+
+@@cart = []
+
+Item.new("Figs",3.42)
+Item.new("Pears",0.99)
+
+def call(env)
+	resp = Rack::Response.new
+	req = Rack::Request.new(env)
+
+	if req.path.match(/items/)
+
+		item_name = req.path.split("/items/").last
+		if item = @@items.find{|item| item.name == item_name}
+			resp.write item.price 
+		else 
+			resp.status = 400
+			resp.write "Item not found"
+		end 
+	
+	else 
+		resp.write "Route not found"
+		resp.status = 404
+	end 
+
+	resp.finish 
+end 
+
+end 
